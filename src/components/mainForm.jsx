ada578a69abe4bc7d8cbf6c41575762f2449f885
Lukas <lukas.ancikevicius@gmail.com>
2021-06-22 08:59:15 +0300

    css component

---
+++ src/components/mainForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class MainForm extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="main-form">
+        <h1>Main form</h1>
+      </div>
+    );
+  }
+}
+
+export default MainForm;
